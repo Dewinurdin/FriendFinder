637c790b0a614eb30e4848e034e7f2230dde58ed
dewinurdin <dewinurdin@gmail.com>
2018-04-30 01:26:12 -0500

    fixed it

--- server.js
+++ server.js
@@ -1,21 +1,43 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-// var path = require('path'); 
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
 var app = express();
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
- 
-// parse application/json
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// BodyParser makes it easy for our server to interpret data sent to it.
+// The code below is pretty standard.
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-//express.static links to public folder and all its content inc css and js
-app.use(express.static('public')); 
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
-var htmlRoutes = require("./routing/htmlRoutes");
-var apiRoutes = require("./routing/apiRoutes");
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
-htmlRoutes(app);  //Linked to HTML Routes
-apiRoutes(app);	//Linked to API Routes
+// ==============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// ==============================================================================
 
-app.listen(3000, () => console.log('App is listening on port 3000!'));
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
